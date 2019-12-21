7abad12e514418701037353712928f95b1cd59e1
Roman <roman.lubij@gmail.com>
2019-12-21 21:29:33 +0100

    Maybe now?

--- scripts/minify.sh
+++ scripts/minify.sh
@@ -4,6 +4,7 @@ cd ../src
 node-minify --compressor csso --input public/light.css --output ../dist/public/light.css
 node-minify --compressor csso --input public/dark.css --output ../dist/public/dark.css
 
-node-minify --compressor google-closure-compiler --input public/script.js --output ../dist/public/script.js
-
 node-minify --compressor html-minifier --input index.html --output ../dist/index.html
+node-minify --compressor html-minifier --input google74b9fdef1bfecd20.html --output ../dist/google74b9fdef1bfecd20.html
+
+node-minify --compressor google-closure-compiler --input public/script.js --output ../dist/public/script.js
