69e4544bbcc1731d4e0875990dfc0c5b9fc62b37
Roman <roman.lubij@gmail.com>
2019-12-21 17:07:50 +0100

    Moved "minified" site to `dist/`

--- scripts/minify.sh
+++ scripts/minify.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 cd ../src
 
-node-minify --compressor csso --input css/light.css --output ../docs/css/light.css
-node-minify --compressor csso --input css/dark.css --output ../docs/css/dark.css
+node-minify --compressor csso --input css/light.css --output ../dist/css/light.css
+node-minify --compressor csso --input css/dark.css --output ../dist/css/dark.css
 
-node-minify --compressor google-closure-compiler --input js/script.js --output ../docs/js/script.js
+node-minify --compressor google-closure-compiler --input js/script.js --output ../dist/js/script.js
 
-node-minify --compressor html-minifier --input index.html --output ../docs/index.html
+node-minify --compressor html-minifier --input index.html --output ../dist/index.html
